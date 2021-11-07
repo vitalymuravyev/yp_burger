e3c47ba5374a7a2c53e27bd6a857f994e523e5c7
vitalymuravyev <vitalymuravyev@gmail.com>
2021-11-07 22:59:59 +0300

    Add app header

---
+++ src/components/app-header/AppHeader.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { Logo, BurgerIcon, ProfileIcon, ListIcon } from '@ya.praktikum/react-developer-burger-ui-components'
+
+import styles from './AppHeader.module.css';
+import { ButtonWithIcon } from '../button-with-icon/ButtonWithIcon';
+
+export class AppHeader extends React.Component {
+    render() {
+        return (
+            <header className={styles.container}>
+                <nav className={styles.menu}>
+                    <ButtonWithIcon type="constructor" active />
+                    <ButtonWithIcon type="list" />
+                </nav>
+                <ButtonWithIcon type="profile" />
+                <span className={styles.logo}>
+                    <Logo />
+                </span>
+            </header>
+        )
+    }
+}
---
+++ src/components/button-with-icon/ButtonWithIcon.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './ButtonWithIcon.module.css';
+import { BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+
+const ICONS = {
+    constructor: <BurgerIcon type="primary" />,
+    list: <ListIcon type="secondary" />,
+    profile: <ProfileIcon type="secondary" />,
+}
+
+const LABELS = {
+    constructor: 'Конструктор',
+    list: 'Лента заказов',
+    profile: 'Личный кабинет',
+}
+
+export class ButtonWithIcon extends React.Component {
+    render() {
+        const { type, active } = this.props;
+        const textColor = active ? '' : 'text_color_inactive'
+        return(
+            <a href="#" className={styles.wrapper}>
+                {ICONS[type]}
+                <p className={`text text_type_main-default ${textColor} ml-2`}>
+                    {LABELS[type]}
+                </p>
+            </a>
+        )
+    }
+}
+
+ButtonWithIcon.propTypes = {
+    type: PropTypes.oneOf(['constructor', 'list', 'profile']).isRequired,
+    active: PropTypes.bool
+}
