09ea6ed0a8f2d7817f91a3c2bc0dbf9ac72a4190
vitalymuravyev <44782070+vitalymuravyev@users.noreply.github.com>
2021-11-15 13:12:59 +0300

    Scroll only ingredients

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -10,29 +10,48 @@ import styles from './burger-constructor.module.css';
 import {PriceBlock} from "../price-block/price-block";
 
 export const BurgerConstructor = ({ data }) => {
+  const bun = data[0];
   return (
     <section className={styles.wrapper}>
-      <ul className={styles.list}>
-        {data.map((item, index, arr) => {
-            const type = index === 0 ? "top" : index === arr.length - 1 ? 'bottom' : '';
-            const isLocked = index === 0 || index === arr.length - 1;
-            const additionalText = index === 0 ? "(верх)" : index === arr.length - 1 ? '(низ)' : '';
-            const text = `${item.name} ${additionalText}`
-            return (
+      <div className={styles.container}>
+        <div className={styles.item}>
+          <ConstructorElement
+            text={`${bun.name} (верх)`}
+            price={bun.price}
+            thumbnail={bun.image}
+            type="top"
+            isLocked
+          />
+        </div>
+        <ul className={styles.list}>
+          {data.filter((item) => item.type !== 'bun').map((item, index, arr) => {
+              // const type = index === 0 ? "top" : index === arr.length - 1 ? 'bottom' : '';
+              // const isLocked = index === 0 || index === arr.length - 1;
+              // const additionalText = index === 0 ? "(верх)" : index === arr.length - 1 ? '(низ)' : '';
+              // const text = `${item.name} ${additionalText}`
+              return (
                 <li key={item._id} className={styles.item}>
-                    {!isLocked && <DragIcon type="primary"/>}
-                    <ConstructorElement
-                        text={text}
-                        price={item.price}
-                        thumbnail={item.image}
-                        type={type}
-                        isLocked={isLocked}
-                    />
+                  {<DragIcon type="primary"/>}
+                  <ConstructorElement
+                    text={item.name}
+                    price={item.price}
+                    thumbnail={item.image}
+                  />
                 </li>
-            )
-        }
-        )}
-      </ul>
+              )
+            }
+          )}
+        </ul>
+        <div className={styles.item}>
+          <ConstructorElement
+            text={`${bun.name} (низ)`}
+            price={bun.price}
+            thumbnail={bun.image}
+            type="bottom"
+            isLocked
+          />
+        </div>
+      </div>
       <div className={styles.order}>
           <PriceBlock count={610} size="medium" />
         <Button size="large">Оформить заказ</Button>
