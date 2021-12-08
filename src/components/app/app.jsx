fbdaa4fc5eb4737034615134000981612d647742
vitalymuravyev <44782070+vitalymuravyev@users.noreply.github.com>
2021-12-08 15:32:18 +0300

    Remove unnecessary context

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,40 +1,18 @@
-import React, {useEffect, useReducer, useState} from 'react';
+import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import styles from './app.module.css';
 
 import { AppHeader } from '../app-header/app-header';
 import { BurgerConstructor } from "../burger-constructor/burger-constructor";
 import {BurgerIngredients} from "../burger-ingredients/burger-ingredients";
-import {BurgerContext} from "../../services/appContext";
 import { ERROR_MESSAGE} from "../../utils/constants";
 import {Modal} from "../modal/modal";
 import {CLOSE_ERROR, getItems} from "../../services/actions/burger-ingredients";
 
-const burgerInitialState = {bun: null, ingredients: []};
-
-function burgerReducer(state, action) {
-  switch (action.type) {
-    case 'add':
-      if (action.payload.type === 'bun') {
-        return ({
-          ...state,
-          bun: action.payload,
-        });
-      }
-      return ({
-        ...state,
-        ingredients: [...state.ingredients, action.payload],
-      });
-    default:
-      console.log('wrong action type');
-  }
-}
 
 function App() {
   const dispatch = useDispatch();
   const errorMessage = useSelector(state => state.ingredients.itemsFailed);
-
-  const [ burger, dispatchBurger ] = useReducer(burgerReducer, burgerInitialState, undefined);
 
   useEffect(() => {
     dispatch(getItems());
@@ -50,10 +28,8 @@ function App() {
     <div className="App">
       <AppHeader />
       <main className={styles.main}>
-        <BurgerContext.Provider value={{burger, dispatchBurger}}>
           <BurgerIngredients />
           <BurgerConstructor />
-        </BurgerContext.Provider>
       </main>
       {errorMessage &&
         <Modal closeModal={closeModal}>
