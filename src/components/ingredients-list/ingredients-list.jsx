1babd371508947fa18ddfb943fbbb6ef14fa9356
vitalymuravyev <44782070+vitalymuravyev@users.noreply.github.com>
2021-12-10 17:11:37 +0300

    Scroll ingredients list

--- src/components/ingredients-list/ingredients-list.jsx
+++ src/components/ingredients-list/ingredients-list.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useEffect} from "react";
+import { useInView } from "react-intersection-observer";
 import PropTypes from 'prop-types';
 
 import {useDispatch, useSelector} from "react-redux";
@@ -10,11 +11,20 @@ import {IngredientDetails} from "../ingredient-details/ingredient-details";
 import {ADD_ITEM_INFO, REMOVE_ITEM_INFO} from "../../services/actions/ingredient-card";
 import {ADD_BURGER_ITEM} from "../../services/actions/burger-constructor";
 
-export const IngredientsList = ({ type }) => {
+export const IngredientsList = ({ type, reference, changeTab }) => {
   const data = useSelector(state => state.ingredients.items);
   const dispatch = useDispatch();
   const { isDetailsVisible } = useSelector(state => state.ingredientInfo);
   const ingredients = data.filter((item) => item.type === type);
+
+  const { ref, inView, entry } = useInView({
+    threshold: [0, 0.25, 0.5, 0.75, 1]
+  });
+
+  useEffect(() => {
+    const ratio = entry ? entry.intersectionRatio : 0;
+    changeTab(type, ratio);
+  }, [entry]);
 
   const closeModal = () => {
     dispatch({
@@ -31,16 +41,18 @@ export const IngredientsList = ({ type }) => {
 
   return (
     <React.Fragment>
-      <div className={styles.wrapper}>
-        <h3 className="text text_type_main-medium">{ingredientTypes[type]}</h3>
-        <div className={styles.list}>
-          {ingredients.map((item) =>
-            <IngredientCard
-              key={item._id}
-              item={item}
-              onClick={() => handleItemClick(item)}
-            />
-          )}
+      <div ref={reference}>
+        <div className={styles.wrapper} ref={ref} >
+          <h3 className="text text_type_main-medium" >{ingredientTypes[type]}</h3>
+          <div className={styles.list}>
+            {ingredients.map((item) =>
+              <IngredientCard
+                key={item._id}
+                item={item}
+                onClick={() => handleItemClick(item)}
+              />
+            )}
+          </div>
         </div>
       </div>
       {isDetailsVisible && <IngredientDetails closeModal={closeModal} />}
@@ -49,5 +61,8 @@ export const IngredientsList = ({ type }) => {
 };
 
 IngredientsList.propTypes = {
-  type: PropTypes.oneOf(['bun', 'sauce', 'main']).isRequired
+  type: PropTypes.oneOf(['bun', 'sauce', 'main']).isRequired,
+  // eslint-disable-next-line react/forbid-prop-types
+  reference: PropTypes.object.isRequired,
+  changeTab: PropTypes.func.isRequired
 };
--- src/components/tab-panel/tab-panel.jsx
+++ src/components/tab-panel/tab-panel.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
+import PropTypes from 'prop-types';
 
 import {Tab} from "@ya.praktikum/react-developer-burger-ui-components";
 import styles from './tab-panel.module.css';
@@ -7,16 +8,20 @@ import { ingredientTypes } from '../../utils/data';
 
 const TABS = ['bun', 'sauce', 'main'];
 
-export const TabPanel = () => {
-  const [currentTab, setCurrentTab] = useState('bun');
+export const TabPanel = ({currentTab, setTab}) => {
 
   return (
     <div className={styles.wrapper}>
       {TABS.map((tab, index) =>
-        <Tab key={index} active={currentTab === tab} value={tab} onClick={setCurrentTab}>
+        <Tab key={index} active={currentTab === tab} value={tab} onClick={setTab}>
           {ingredientTypes[tab]}
         </Tab>
       )}
     </div>
   );
+};
+
+TabPanel.propTypes = {
+  currentTab: PropTypes.string.isRequired,
+  setTab: PropTypes.func.isRequired
 };
