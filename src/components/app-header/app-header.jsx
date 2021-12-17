7c469fed4557b8aa481f5c724a8787270490d33c
vitalymuravyev <vitalymuravyev@gmail.com>
2021-12-17 22:51:32 +0300

    Header active links

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 
@@ -12,24 +13,42 @@ const LABELS = {
 };
 
 export const AppHeader = () => {
-    return (
-        <header className={styles.container}>
-          <div className={styles.wrapper}>
-            <nav className={styles.menu}>
-              <ButtonWithIcon active icon={<BurgerIcon type="primary" />} path="/">
-                {LABELS.constructor}
-              </ButtonWithIcon>
-              <ButtonWithIcon icon={<ListIcon type="secondary" />} path="/order-feed">
-                {LABELS.list}
-              </ButtonWithIcon>
-            </nav>
-            <ButtonWithIcon icon={<ProfileIcon type="secondary" /> } path="/login">
-              {LABELS.profile}
-            </ButtonWithIcon>
-            <span className={styles.logo}>
-              <Logo />
-          </span>
-          </div>
-        </header>
-    );
+  const { pathname } = useLocation();
+
+  const isActive = (path) => {
+    return path === pathname;
+  };
+
+  return (
+    <header className={styles.container}>
+      <div className={styles.wrapper}>
+        <nav className={styles.menu}>
+          <ButtonWithIcon
+            active={isActive("/")}
+            icon={<BurgerIcon type={isActive("/") ? "primary" : "secondary"} />}
+            path="/"
+          >
+            {LABELS.constructor}
+          </ButtonWithIcon>
+          <ButtonWithIcon
+            active={isActive("/order-feed")}
+            icon={<ListIcon type={isActive("/order-feed") ? "primary" : "secondary"} />}
+            path="/order-feed"
+          >
+            {LABELS.list}
+          </ButtonWithIcon>
+        </nav>
+        <ButtonWithIcon
+          active={isActive("/login")}
+          icon={<ProfileIcon type={isActive("/login") ? "primary" : "secondary"} /> }
+          path="/login"
+        >
+          {LABELS.profile}
+        </ButtonWithIcon>
+        <span className={styles.logo}>
+          <Logo />
+      </span>
+      </div>
+    </header>
+  );
 };
--- src/components/button-with-icon/button-with-icon.jsx
+++ src/components/button-with-icon/button-with-icon.jsx
@@ -7,7 +7,6 @@ import styles from './button-with-icon.module.css';
 export const ButtonWithIcon = ({ active, children, icon, path }) => {
     const textColor = active ? '' : 'text_color_inactive';
     return(
-        // eslint-disable-next-line jsx-a11y/anchor-is-valid
         <Link to={path} className={styles.wrapper}>
             {icon}
             <p className={`text text_type_main-default ${textColor} ml-2`}>
@@ -18,7 +17,7 @@ export const ButtonWithIcon = ({ active, children, icon, path }) => {
 };
 
 ButtonWithIcon.propTypes = {
-    active: PropTypes.bool,
+    active: PropTypes.bool.isRequired,
     children: PropTypes.string.isRequired,
     icon: PropTypes.element.isRequired,
     path: PropTypes.string.isRequired
